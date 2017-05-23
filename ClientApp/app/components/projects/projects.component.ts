23ee6ef51d5185b6aff64703e7ea6784e154190f
arrow-dev <4rrow88@gmail.com>
2017-05-23 23:49:43 +1200

    Fetch project data and render dynamically

--- ClientApp/app/components/projects/projects.component.ts
+++ ClientApp/app/components/projects/projects.component.ts
@@ -1,6 +1,6 @@
 import { ProjectService } from './../../services/project.service';
 import { Project } from './../../models/project';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
@@ -11,4 +11,9 @@ export class ProjectsComponent {
     public projects: Project[];
 
     constructor(private projectService: ProjectService) { };
+
+    ngOnInit() {
+        this.projectService.getProjects().subscribe(projects =>
+            this.projects = projects);
+    }
 }
